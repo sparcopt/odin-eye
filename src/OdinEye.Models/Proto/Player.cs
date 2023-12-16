92754a322c119bfc8db4b835a7fe4a1d5e6cc0a2
sparcopt <j.l.p.almeida42@gmail.com>
2023-12-16 22:56:37 +0000

    Add player data to player events

---
+++ src/OdinEye.Models/Proto/Player.cs
@@ -0,0 +1,26 @@
+﻿namespace OdinEye.Models.Proto
+{
+    using ProtoBuf;
+
+    [ProtoContract]
+    public class Player
+    {
+        [ProtoMember(1)]
+        public string Id { get; set; }
+        
+        [ProtoMember(2)]
+        public string SteamId { get; set; }
+        
+        [ProtoMember(3)]
+        public string Name { get; set; }
+        
+        [ProtoMember(4)]
+        public float Health { get; set; }
+        
+        [ProtoMember(5)]
+        public float MaxHealth { get; set; }
+        
+        [ProtoMember(6)]
+        public float Stamina { get; set; }
+    }
+}
---
+++ src/OdinEye/Extensions/ZNetPeerExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace OdinEye.Extensions
+{
+    using Models.Proto;
+
+    public static class ZNetPeerExtensions
+    {
+        public static Player ToPlayer(this ZNetPeer peer)
+        {
+            var zdo = ZDOMan.instance.GetZDO(peer.m_characterID);
+            zdo.GetFloat(ZDOVars.s_health, out var health);
+            zdo.GetFloat(ZDOVars.s_maxHealth, out var maxHealth);
+            zdo.GetFloat(ZDOVars.s_stamina, out var stamina);
+                            
+            return new Player
+            {
+                Id = peer.m_characterID.ToString(),
+                Name = peer.m_playerName,
+                SteamId = peer.m_socket.GetHostName(),
+                Health = health,
+                MaxHealth = maxHealth,
+                Stamina = stamina
+            };
+        }
+    }
+}
