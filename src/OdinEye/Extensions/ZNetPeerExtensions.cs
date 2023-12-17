eaec6208a6becdb50bbf442432a876c5ed04da1b
sparcopt <j.l.p.almeida42@gmail.com>
2023-12-17 01:03:21 +0000

    Remove player stats
    Stats are still not available right after a player joins

--- src/OdinEye/Extensions/ZNetPeerExtensions.cs
+++ src/OdinEye/Extensions/ZNetPeerExtensions.cs
@@ -4,22 +4,12 @@
 
     public static class ZNetPeerExtensions
     {
-        public static Player ToPlayer(this ZNetPeer peer)
-        {
-            var zdo = ZDOMan.instance.GetZDO(peer.m_characterID);
-            zdo.GetFloat(ZDOVars.s_health, out var health);
-            zdo.GetFloat(ZDOVars.s_maxHealth, out var maxHealth);
-            zdo.GetFloat(ZDOVars.s_stamina, out var stamina);
-                            
-            return new Player
+        public static Player ToPlayer(this ZNetPeer peer) =>
+            new Player
             {
                 Id = peer.m_characterID.ToString(),
                 Name = peer.m_playerName,
-                SteamId = peer.m_socket.GetHostName(),
-                Health = health,
-                MaxHealth = maxHealth,
-                Stamina = stamina
+                SteamId = peer.m_socket.GetHostName()
             };
-        }
     }
 }
