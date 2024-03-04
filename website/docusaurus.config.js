f7ddd0287f4fd2454657f0c2555c78f39ba5aa6c
sparcopt <j.l.p.almeida42@gmail.com>
2024-03-04 23:18:25 +0000

    Fix repository name

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sparcopt', // Usually your GitHub org/user name.
-  projectName: 'odineye', // Usually your repo name.
+  projectName: 'odin-eye', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
